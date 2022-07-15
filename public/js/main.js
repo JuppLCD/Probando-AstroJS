4105545d37203ca9204cf30a3179d17868535493
JuppLCD <lorenzocanovas207@gmail.com>
2022-07-15 20:22:44 -0300

    Frist time Astro JS, My Portafolio

---
+++ public/js/main.js
@@ -0,0 +1,85 @@
+const proyectInfo = [
+	{
+		title: 'Harry-Potter',
+		description:
+			'Esta App Web esta hecha utilizando React + Typescript utilizando Vite en el frontend, el cual consume una api de <a style="color: #46ff57;" href="https://aztro.readthedocs.io/" target="_blank">Aztro</a> para tema del horoscopo y una api que hice utilizando json-server(<a style="color: #46ff57;" href="https://github.com/JuppLCD/Json-Server-HarryPotter" target="_blank">Code</a>) para obtener los datos de los personajes, frases y libros de la saga de Harry Potter.',
+		demo: 'https://harry-potter-react-ts.netlify.app/',
+		code: 'https://github.com/JuppLCD/Harry-Potter-ReactTS',
+		keywords: 'Web App, React, Typescript, Bootstrap 5, Api de Harry Potter, json-server, NodeJS',
+	},
+	{
+		title: 'Movies-Frontend',
+		description:
+			'Esta App Web esta hecha sobre una "plantilla" de una lista de reproduccion de "Yoelvis Mulen { code }" sobre aprender React. <br/><br/> Lo modifique al agregarle Typescript utilizando Vite en el frontend, esta consume una api de <a style="color: #46ff57;" href="https://www.themoviedb.org/" target="_blank">TheMovieDB</a> para obtener los datos de las peliculas, utilizando Bootstrap 5 para facilitar el diseño y un poco de Sass para modificar un poco las variables de esta. Agrege un login, para que el usuario tenga las funcionalidades de un CRUD mediante listas de peliculas. Ademas utilize una libreria de notificaciones para informar al usuario de sus acciones.',
+		demo: 'https://movies-jupplcd.netlify.app/',
+		code: 'https://github.com/JuppLCD/Movies-Frontend',
+		keywords: 'Web App, React, Typescript, Bootstrap 5, Api de TheMovieDB, Sass, React-Hot-Toas',
+	},
+	{
+		title: 'Movies-Backend',
+		description:
+			'Es un servidor en NodeJS utilizando Express y un ORM (Sequelize) para manejar la DB en MySQL. <br/> Esta contiene una api para manejar usuarios (login, singup) utilizando JWT para manejar la cesion y bcryptjs para encriptar la contraseña del usuario antes de guardarla en la DB, ademas de una api tipo CRUD respecto a listas de peliculas para la App Web Movies que se puede ver en la demo.',
+		demo: 'https://movies-jupplcd.netlify.app/',
+		code: 'https://github.com/JuppLCD/Movies-app-backend',
+		keywords: 'NodeJS, Express, hapi/boom, Joi (validator Schema), MySQL, Sequelize, jsonwebtoken, bcryptjs',
+	},
+	{
+		title: 'ToDo',
+		description:
+			'Es un proyecto simple, pero lo coloco por mero gusto, ya que fue el primer proyecto en la que programe tanto en frontend como en backend aunque es poquitio el codigo, podria decir que fue un paso inportante para meteme mas en el lindo mundo de la programacion &#128518;&#128518;. <br/>Como se puede ver es un  ToDo utilizando React en Frontend y en Backend(<a style="color: #46ff57;" href="https://github.com/JuppLCD/todo-app-backend" target="_blank">Code</a>) utilizando NodeJS con Express y Mongoose para conectarme a una base de datos en MongoDB',
+		demo: 'https://todo-react-crud.herokuapp.com/',
+		code: 'https://github.com/JuppLCD/react-todo',
+		keywords: 'Web App, React, NodeJS, Express, MongoDB',
+	},
+	{
+		title: 'Rick-and-Morty',
+		description:
+			'Es una App Web hecha en React consumiendo la api de Rick and Morty para practicar la paginacion, el scroll infinite y Search Query',
+		demo: 'https://rick-and-morty-jupplcd.netlify.app/',
+		code: 'https://github.com/JuppLCD/rick-and-morty',
+		keywords: 'Web App, React, Api Rick and Morty, Bootstrap 5',
+	},
+	{
+		title: 'Chat-App-Sockets',
+		description:
+			'El codigo original de esta app no ha sido creada por mi, esta a sido modificada y refactorizada por mi. Mas info en el README del repositorio de este proyecto en github. <br/> <br/> Basicamente esta app utilizaba http para hacer el pedido al backend en PHP y lo cambie a Sockets con la libreria Reatchet, ademas queria usar POO por lo que pase mucha logia a Clases (ya que nunca lo habia utilizado), aunque lo hice sin mucha idea de las buenas practicas de POO en PHP <b> (me encuentro mejorando en el POO a la vez que estoy aprendiendo Laravel)</b>',
+		demo: 'https://github.com/JuppLCD/Chat-App-PHP',
+		code: 'https://github.com/JuppLCD/Chat-App-PHP',
+		keywords: 'PHP, MySQL, Ratchet, Sockets, Session',
+	},
+];
+
+const portafolio = document.getElementById('portafolio');
+
+const modal = document.getElementById('proyectInfoModal');
+const template = document.getElementById('proyectInfoTemplate').content;
+
+portafolio.addEventListener('click', (e) => {
+	if (
+		e.target.tagName === 'ARTICLE' ||
+		e.target.parentNode.tagName === 'ARTICLE' ||
+		e.target.parentNode.parentNode.tagName === 'ARTICLE' ||
+		e.target.parentNode.parentNode.parentNode.tagName === 'ARTICLE'
+	) {
+		let articleElement = e.target;
+		while (articleElement.tagName !== 'ARTICLE') {
+			articleElement = articleElement.parentNode;
+		}
+		const fragment = document.createDocumentFragment();
+
+		const titleProyect = articleElement.dataset.proyect;
+		const data = proyectInfo.filter((proyect) => proyect.title === titleProyect)[0];
+
+		template.querySelector('.modal-title span').textContent = data.title;
+		template.querySelector('.modal-body h6').textContent = data.keywords;
+		template.querySelector('.modal-body p').innerHTML = data.description;
+		template.querySelector('.modal-body a.btn-warning').href = data.demo;
+		template.querySelector('.modal-body a.btn-outline-light').href = data.code;
+
+		const clone = template.cloneNode(true);
+		fragment.appendChild(clone);
+
+		modal.querySelector('.modal-dialog').innerHTML = '';
+		modal.querySelector('.modal-dialog').appendChild(fragment);
+	}
+});
